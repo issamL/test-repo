1c7f3d29b22f750a4ff8f6aeeeaa784fba5be323
issamL <lhous.issam99@gmail.com>
2021-06-03 14:48:16 +0200

    change

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,15 +1,18 @@
 const LoginPage = require('../pageobjects/login.page');
-
+new LoginPage();
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
-        await LoginPage.open('login.html');
-        await browser.pause( 2000 );
-        await LoginPage.login('myuser', 'az12');
-        await browser.pause( 2000 );
+
+        await browser.url('http://localhost/repo/app/login.html');
+        await browser.pause( 5000 );
+        await browser.element("#username").setValue('myuser');
+        await $('#pwd').setValue('az12');
+        await $('#signInSubmit').click();
+        //await LoginPage.login('myuser', 'az12');
+        await browser.pause( 5000 );
         const elem = $('#fh5co-logo');
         await browser.pause( 2000 );
-        await expect(elem).toHaveTextContaining(
-            'SH4RE');
+        await expect(elem).toHaveTextContaining('SH4RE');
     });
 });
 
