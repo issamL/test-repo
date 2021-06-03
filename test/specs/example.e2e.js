1ca463fa1129962ae72c716ec2b81b3571c7cc21
issamL <lhous.issam99@gmail.com>
2021-06-03 12:29:35 +0200

    changing test script by adding user as variable

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -4,12 +4,12 @@ describe('My Login application', () => {
     it('should login with valid credentials', async () => {
 
         await browser.url('http://localhost:8080/home/circleci/repo/app/login.html');
-        await browser.pause( 2000 );
+        await browser.pause( 5000 );
         await $('#username').setValue('myuser');
         await $('#pwd').setValue('az12');
         await $('#signInSubmit').click();
         //await LoginPage.login('myuser', 'az12');
-        await browser.pause( 2000 );
+        await browser.pause( 5000 );
         const elem = $('#fh5co-logo');
         await browser.pause( 2000 );
         await expect(elem).toHaveTextContaining('SH4RE');
