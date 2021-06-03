d4719fb474fd5994d8a48c2be19913cbf9d055a5
issamL <lhous.issam99@gmail.com>
2021-06-03 18:00:37 +0200

    change ci config file

--- wdio.conf.js
+++ wdio.conf.js
@@ -97,7 +97,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'http://localhost/',
+    baseUrl: 'http://127.0.0.1/www/test-repo/',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
