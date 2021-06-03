e5cd2b37b31a3f0ecee30e4c8eb81cc4c6223b0d
issamL <lhous.issam99@gmail.com>
2021-06-03 17:34:58 +0200

    change url and config file

--- test/pageobjects/page.js
+++ test/pageobjects/page.js
@@ -8,7 +8,7 @@ module.exports = class Page {
     * @param path path of the sub page (e.g. /path/to/page.html)
     */
     open (path) {
-        return browser.url(`http://localhost/www/app/${path}`);
+        return browser.url(`http://127.0.0.1/www/html/test-repo/app/${path}`);
     }
 }
 
