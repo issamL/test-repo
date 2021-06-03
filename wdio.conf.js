925881dbd832cd988f34148a6cb28b3e893cd1f5
issamL <lhous.issam99@gmail.com>
2021-06-03 13:55:28 +0200

    change conf

--- wdio.conf.js
+++ wdio.conf.js
@@ -74,7 +74,7 @@ exports.config = {
     //
     // Set a base URL in order to shorten url command calls. If your url parameter starts
     // with "/", then the base url gets prepended.
-    baseUrl: 'http://localhost:8080',
+    baseUrl: 'http://localhost/repo/app/login.html',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
