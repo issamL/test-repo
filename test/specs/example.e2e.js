fad800dba03779b9e8436fdf1a48d34807e875d0
issamL <lhous.issam99@gmail.com>
2021-06-03 18:24:22 +0200

    changing url and ci config

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -3,7 +3,7 @@ const LoginPage = require('../pageobjects/login.page');
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
         await LoginPage.open('login.html');
-        await browser.pause( 10000 );
+        await browser.pause( 2000 );
         await LoginPage.login('myuser', 'az12');
         await browser.pause( 2000 );
         const elem = $('#fh5co-logo');
